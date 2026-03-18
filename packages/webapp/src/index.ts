c2298d7a117d1661aaace00d5d81cc5838e5f029
Pankaj Kumar <pankaj.kumar.phe21@itbhu.ac.in>
2026-03-18 13:41:46 +0530

    refactor(ui-components): consolidate UI component exports into a single barrel file

--- packages/webapp/src/index.ts
+++ packages/webapp/src/index.ts
@@ -4,34 +4,9 @@
  */
 export * from './utils/util';
 
-// UI components (ui-components folder and subfolders)
-export * from './ui-components/ui/accordion';
-export * from './ui-components/ui/alert';
-export * from './ui-components/ui/alert-dialog';
-export * from './ui-components/ui/aspect-ratio';
-export * from './ui-components/ui/auto-resize-textarea';
-export * from './ui-components/ui/avatar';
-export * from './ui-components/ui/breadcrumb';
-export * from './ui-components/ui/button';
-export * from './ui-components/ui/button-group';
-export * from './ui-components/ui/card';
-export * from './ui-components/ui/carousel';
-export * from './ui-components/ui/chart';
-export * from './ui-components/ui/checkbox';
-export * from './ui-components/ui/collapsible';
-export * from './ui-components/ui/command';
-export * from './ui-components/ui/context-menu';
-export * from './ui-components/ui/data-table-pagination';
-export * from './ui-components/ui/dialog';
-export * from './ui-components/ui/document-list';
-export * from './ui-components/ui/drawer';
-export * from './ui-components/ui/dropdown-menu';
-export * from './ui-components/ui/pagination';
-export * from './ui-components/ui/popover';
-export * from './ui-components/ui/select';
-export * from './ui-components/ui/separator';
-export * from './ui-components/ui/textarea';
-export * from './ui-components/ui/dashboard-grid-layout';
-export * from './ui-components/audio/player';
-export * from './ui-components/audio/recorder';
+export * from "./ui-components";
+
+// lib utilities
+export * from './lib/toast';
+export * from './lib/utils';
 
---
+++ packages/webapp/src/ui-components/index.ts
@@ -0,0 +1,52 @@
+// Re-export all UI components from this folder and subfolders.
+// Keep this as the single barrel for ui-components.
+
+export * from "./ui/accordion";
+export * from "./ui/alert";
+export * from "./ui/alert-dialog";
+export * from "./ui/aspect-ratio";
+export * from "./ui/auto-resize-textarea";
+export * from "./ui/avatar";
+export * from "./ui/breadcrumb";
+export * from "./ui/button";
+export * from "./ui/button-group";
+export * from "./ui/card";
+export * from "./ui/carousel";
+export * from "./ui/chart";
+export * from "./ui/checkbox";
+export * from "./ui/collapsible";
+export * from "./ui/command";
+export * from "./ui/context-menu";
+export * from "./ui/data-table-pagination";
+export * from "./ui/dialog";
+export * from "./ui/document-list";
+export * from "./ui/drawer";
+export * from "./ui/dropdown-menu";
+export * from "./ui/pagination";
+export * from "./ui/popover";
+export * from "./ui/select";
+export * from "./ui/separator";
+export * from "./ui/textarea";
+export * from "./ui/dashboard-grid-layout";
+
+export * from "./audio/player";
+export * from "./audio/recorder";
+
+export * from "./common/audioGrid";
+export * from "./common/audioPlayer";
+export * from "./common/audioVisualizer";
+export * from "./common/calendarCard";
+export * from "./common/customCasCader";
+export * from "./common/customVisualizer";
+export * from "./common/dataTable";
+export * from "./common/documentPreview";
+export * from "./common/ganttChart";
+export * from "./common/imagePreview";
+export * from "./common/kpiCards";
+export * from "./common/Recaptcha";
+export * from "./common/RecaptchaVerificationScreen";
+export * from "./common/tablePagination";
+export * from "./common/videoGrid";
+export * from "./common/videopreview";
+export * from "./common/voiceTextInput";
+
